4ba7d700966eea84f3be4b1a3ef27116c5b39b1a
BalmDeveloper <balmtechnologies@gmail.com>
2026-04-02 09:30:19 -0700

    Add portfolio page with client logos, improve mobile experience, update navigation

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,19 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        insights: './insights.html',
+        article: './insights/the-gift-of-naming-things.html',
+        portfolio: './portfolio.html'
+      }
+    }
+  },
+  server: {
+    fs: {
+      allow: ['..']
+    }
+  }
 });
